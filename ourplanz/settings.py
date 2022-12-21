0e3ebfd2a76a2f4e0392c102f1a1724bb7e63d5e
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2022-12-22 00:59:27 +0330

    Change database to MySQL

--- ourplanz/settings.py
+++ ourplanz/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'ourplanz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ourplanz',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'admin'
     }
 }
 
